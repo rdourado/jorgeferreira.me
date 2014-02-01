288b97bbc3834627c696b961ba4aee509874a8b1
Rafael Dourado <rdourado@gmail.com>
2014-02-01 02:20:06 -0200

    Now playing with Bootstrap and jQuery.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,26 +1,5 @@
 /*
-@codekit-prepend "lib/ui-bootstrap-custom-tpls-0.10.0.min.js"
-global "angular";
+@codekit-prepend "bootstrap/collapse.js"
+@codekit-prepend "bootstrap/dropdown.js"
+@codekit-prepend "bootstrap/tooltip.js"
 */
-
-var myApp = angular.module('jorgeApp', ['ui.bootstrap']);
-
-myApp.controller('AppCtrl', ['$scope', '$window', function($scope, $window) {
-	var bp = 768;
-
-	$scope.getWidth = function() {
-		return $window.document.width;
-	};
-	$scope.$watch($scope.getWidth, function(newValue, oldValue) {
-		if (oldValue >= bp && newValue < bp) 
-			$scope.isXS = true;
-		else if (oldValue < bp && newValue >= bp)
-			$scope.isXS = false;
-	});
-	window.onresize = function() {
-		$scope.$apply();
-	};
-
-	$scope.isXS = ($scope.getWidth() < bp);
-	$scope.showAbout = false;
-}]);
